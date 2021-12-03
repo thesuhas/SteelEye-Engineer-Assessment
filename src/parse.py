45ca23e071d509b27d4d1d2670bdedd30d2ab1cf
Suhas Thalanki <suhastvs@gmail.com>
2021-12-04 00:53:47 +0530

    test

---
+++ src/parse.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import xml.etree.ElementTree as Tree
+import os
+
+def parse(path):
+    # Getting xml tree
+    tree = Tree.parse(path)
+    # Get the root of tree
+    root = tree.getroot()
+    
+    parent = 'TermntdRcrd'
+
+    pattern = 'FinInstrmGnlAttrbts'
+
+    tag = 'Issr'
+
+    children = ['Id', 'FullNm', 'ClssfctnTp', 'CmmdtyDerivInd', 'NtnlCcy']
+    cols = [pattern + '.' + k for k in children]
+    cols.append(tag)
+    
+    rows = list()
+    for i in root.iter():
+        if parent in i.tag:
+            entry = [None for x in range(len(children) + 1)]
+            for child in i:
+                
+                if pattern in child.tag:
+                    #print(child.tag)
+                    
+                    for c in child:
+                        #print(c.tag, c.text)
+                        for k in range(len(children)):
+                            
+                            if children[k] in c.tag:
+                                entry[k] = c.text
+                if tag in child.tag:
+                    #print(child.tag, child.text)
+                    entry[5] = child.text
+            rows.append(entry)
+
+    df = pd.DataFrame(data=rows, columns=cols)
+    df.to_csv('../ans.csv', index=False)    
+            
+
+
+if __name__ == '__main__':
+    
+    path = os.path.join(os.pardir, "DLTINS_20210117_01of01.xml")
+    
+    parse(path)
