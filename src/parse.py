f826d760076a51729002cd4c5ad8a4b1e6a6bd60
Suhas Thalanki <suhastvs@gmail.com>
2021-12-04 01:11:02 +0530

    did some pep8

--- src/parse.py
+++ src/parse.py
@@ -2,6 +2,7 @@ import pandas as pd
 import xml.etree.ElementTree as Tree
 import os
 
+
 def parse(path):
     # Getting xml tree
     tree = Tree.parse(path)
@@ -23,25 +24,18 @@ def parse(path):
         if parent in i.tag:
             entry = [None for x in range(len(children) + 1)]
             for child in i:
-                
                 if pattern in child.tag:
-                    #print(child.tag)
-                    
-                    for c in child:
-                        #print(c.tag, c.text)
+                    for c in child:                        
                         for k in range(len(children)):
                             
                             if children[k] in c.tag:
                                 entry[k] = c.text
-                if tag in child.tag:
-                    #print(child.tag, child.text)
+                if tag in child.tag:                    
                     entry[5] = child.text
             rows.append(entry)
 
     df = pd.DataFrame(data=rows, columns=cols)
     df.to_csv('../ans.csv', index=False)    
-            
-
 
 if __name__ == '__main__':
     
